666e6299d0f894dfb3b44cd2eabf5b27379c6ba3
baitercell <mattoji@hotmail.com>
2015-02-08 20:54:10 +1300

    added new features
    teleports player to spawn
    plays music

--- src/baitercell/leather/OwnEventListener.java
+++ src/baitercell/leather/OwnEventListener.java
@@ -1,6 +1,8 @@
 package baitercell.leather;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,17 +22,29 @@ public final class OwnEventListener implements Listener {
 
 	@EventHandler
 	public void PLE (PlayerJoinEvent event){
+		
+		//set Player to be wearing leather armour
+		
+		//create item stacks with the leather armour
 		ItemStack is1 = new ItemStack(Material.LEATHER_BOOTS, 1);
 		ItemStack is2 = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
 		ItemStack is3 = new ItemStack(Material.LEATHER_LEGGINGS, 1);
 		
+		//assign the leather armour to the correct inv spaces
 		event.getPlayer().getInventory().setBoots(is1);
 		event.getPlayer().getInventory().setChestplate(is2);
-		event.getPlayer().getInventory().setLeggings(is3); 
+		event.getPlayer().getInventory().setLeggings(is3);
+		
+		//force the player to always spawn at spawn in the correct world
+		Location spawn = new Location(Bukkit.getWorld("world"), 651.5, 17, -815.5);
+		event.getPlayer().teleport(spawn);
+		
+		//play mall to every player
+		event.getPlayer().playEffect(event.getPlayer().getLocation(), Effect.RECORD_PLAY, Material.RECORD_6);
 	}
 	
 	@EventHandler
-	public void PIE (InventoryClickEvent event){
+	public void ICE (InventoryClickEvent event){
 		if(event.getRawSlot() == 8 || event.getRawSlot() == 7 || event.getRawSlot() == 6){
 			event.setCancelled(true);
 		}
